9ab4ea8410b4ebaea7fe136a4d9c3c7d2a958628
Marni Hager <mlh6118@gmail.com>
2022-07-13 22:21:17 -0700

    Completed code functionality for Lab 16.

---
+++ README.md
@@ -0,0 +1,15 @@
+# LAB - Class 16
+## Project: Serverless Functions
+### Author: Marni Hager
+
+### Setup
+Setup requires that the user have the following installed, as shown in the 
+requirements.txt file.  
+certifi==2022.6.15  
+charset-normalizer==2.1.0  
+idna==3.3  
+requests==2.28.1  
+urllib3==1.26.10  
+
+### Running the Program
+The user needs to go to 
--- api/capital-finder.py
+++ api/capital-finder.py
@@ -5,6 +5,14 @@ import requests
 class handler(BaseHTTPRequestHandler):
 	# This is a built-in method in BaseHTTPRequestHandler.  Hence, caps.
 	def do_GET(self):
+		"""
+		Arguments:
+			Country: name of country via url (optional)
+			Capital: name of capital city via url (optional)
+
+		Returns:
+			Encoded message
+		"""
 		s = self.path
 		url_components = parse.urlsplit(s)
 		query_string_list = parse.parse_qsl(url_components.query)
@@ -13,10 +21,22 @@ class handler(BaseHTTPRequestHandler):
 		country = dic.get('country')
 		capital = dic.get('capital')
 
-		if country:
-			requests.get('https://restcountries.com/v3.1/name/{country}')
-		elif capital:
-			requests.get('https://restcountries.com/v3.1/name/{capital}')
+		if "country" in dic:
+			r = requests.get(f'https://restcountries.com/v3.1/name/{country}')
+			data = r.json()
+			for details in data:
+				capital_city = details["capital"][0]
+			message = str(f'The capital of {country} is {capital_city}')
+		elif "capital" in dic:
+			r = requests.get(f'https://restcountries.com/v3.1/capital/{capital}')
+			data = r.json()
+			# Use this format since the name data is a dictionary within the
+			# json array.  It will not work calling it from within a for loop
+			# with this syntax.
+			country_name = data[0]["name"]["official"]
+			message = str(f'{capital} is the capital of {country_name}')
+		else:
+			message = str('Please enter a country or capital in the url.')
 
 		self.send_response(200)
 		self.send_header('Content-type', 'text/plain')
